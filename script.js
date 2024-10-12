ba0bbd420a65854de97c90a8c4291296d1987bac
Crypted Hashes <hashesed@gmail.com>
2024-10-12 23:37:10 +0000

    home page redesigned

---
+++ script.js
@@ -0,0 +1,9 @@
+// const slides = document.querySelectorAll('.slide-images img');
+// let index = 0;
+
+// function slideShow() {
+//     index = (index + 1) % slides.length;
+//     document.querySelector('.slide-images').style.transform = `translateX(-${index * 100}%)`;
+// }
+
+// setInterval(slideShow, 5000); // Change slides every 5 seconds
